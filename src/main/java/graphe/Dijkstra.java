4d6c75c157a374f9141e352a73313768a2960cd8
Mei-ling <165828868+Mei-lingWu@users.noreply.github.com>
2024-05-25 23:28:24 +0200

    Update Dijkstra.java

--- src/main/java/graphe/Dijkstra.java
+++ src/main/java/graphe/Dijkstra.java
@@ -4,40 +4,37 @@ import java.util.*;
 
 public class Dijkstra {
     public static void dijkstra(IGraphe g, String source, Map<String, Integer> dist, Map<String, String> prev) {
-        // initialise les structures de donnees
-        PriorityQueue<String> queue = new PriorityQueue<>();
-        Set<String> visite = new HashSet<>();
+        PriorityQueue<String> queue = new PriorityQueue<>(Comparator.comparingInt(dist::get));
 
         // initialise les distances et la file
-        for (String sommet : g.getSommets()) {
-            dist.put(sommet, Integer.MAX_VALUE);
-            prev.put(sommet, null);
+        for (String noeud : g.getSommets()) {
+            dist.put(noeud, Integer.MAX_VALUE);
+            prev.put(noeud, null);
         }
         dist.put(source, 0);
         queue.add(source);
 
         while (!queue.isEmpty()) {
-            String u = queue.poll();
-            if (!visite.add(u)) {
-                continue; // nœud a déjà visite passe au suivant
+            String noeudactuel = queue.poll();
+            int currentDistance = dist.get(noeudactuel);
+
+            // sdistance actuelle est infinie, sort de la boucle
+            if (currentDistance == Integer.MAX_VALUE) {
+                break;
             }
 
-            // met a jour les distances des sommets voisins
-            for (String v : g.getSommets()) {
-                if (g.contientArc(u, v)) {
-                    int valuation = g.getValuation(u, v);
-                    int distsommet = dist.get(u) + valuation;
-                    if (distsommet < dist.get(v)) {
-                        dist.put(v, distsommet);
-                        prev.put(v, u);
-                        queue.add(v);
-                    }
+            // parcourir les successeurs du noeud actuel
+            for (String successeur : g.getSucc(noeudactuel)) {
+                int newDistance = currentDistance + g.getValuation(noeudactuel, successeur);
+                if (newDistance < dist.get(successeur)) {
+                    dist.put(successeur, newDistance);
+                    prev.put(successeur, noeudactuel);
+                    queue.add(successeur);
                 }
             }
         }
-        for (String noeud : g.getSommets())
-            if (dist.get(noeud)==Integer.MAX_VALUE){
-                dist.remove(noeud);
-            }
+
+        // Supprimer les nœuds inaccessibles (distances infinies)
+        dist.entrySet().removeIf(entry -> entry.getValue() == Integer.MAX_VALUE);
     }
 }
